595b7fed1b24b5a2cf72e394e3af545ff135f29c
aaronw176 <aaronw176@nycstudents.net>
2026-01-28 09:23:26 -0500

    day 1

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+def rectangle(x):
+    forward(x)
+    left(90)
+    forward(100)
+    left(90)
+    forward(x)
+    left(90)
+    forward(100)
+    left(90)
+rectangle(125)
+def triangle(x):
+    forward(x)
+    left(120)
+    forward(x)
+    left(120)
+    forward(x)
+triangle(90)    
+turtle.done()
